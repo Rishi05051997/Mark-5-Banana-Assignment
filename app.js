9026c6900c670ba1b886454daf18279c91c00cd2
rishi05051997 <vrushabhdhatral10@gmail.com>
2021-07-25 14:54:37 +0530

    minions completed

---
+++ app.js
@@ -0,0 +1,38 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+var divOutput = document.querySelector("#output");
+
+var urlTranslate = 'https://api.funtranslations.com/translate/pirate.json';
+
+function translationUrl(text) {
+    debugger
+    return urlTranslate + "?" + "text=" + text
+};
+
+// function errorHandler(error) {
+//     console.log("error occured", error);
+//     alert("Something went wrong! Try again after some time ");
+// }
+
+  clickHandler = async() => {
+    
+    var inputText = textInput.value; //input
+
+    //calling server for response
+    debugger;
+    fetch(`https://api.funtranslations.com/translate/pirate.json?text=${inputText}`)
+        
+        .then(response => response.json())
+        .then(json => {
+            // console.log(json)
+            var translatedText = json.contents.translated;
+            divOutput.innerText = translatedText // output
+        })
+    //     .catch(errorHandler);
+    // debugger;
+    // const fetcheddata = await fetch(translationUrl(inputText))
+    // const data = await fetcheddata.json();
+    // console.log(data)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
